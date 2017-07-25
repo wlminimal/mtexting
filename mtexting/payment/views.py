2012cb8c3bf7f42f77b0c92448bebe2cf9a543f6
Taehwan Kim <wlminimal@gmail.com>
2017-07-25 09:49:59 -0700

    comment out try block in checkout view

--- mtexting/payment/views.py
+++ mtexting/payment/views.py
@@ -24,21 +24,20 @@ def checkout(request):
             err = body['error']
         except stripe.error.RateLimitError as e:
             # Too many requests made to the API too quickly
-          pass
+
         except stripe.error.InvalidRequestError as e:
           # Invalid parameters were supplied to Stripe's API
-          pass
+
         except stripe.error.AuthenticationError as e:
           # Authentication with Stripe's API failed
           # (maybe you changed API keys recently)
-          pass
+
         except stripe.error.APIConnectionError as e:
           # Network communication with Stripe failed
-          pass
+
         except stripe.error.StripeError as e:
           # Display a very generic error to the user, and maybe send
           # yourself an email
-          pass
+
         except Exception as e:
           # Something else happened, completely unrelated to Stripe
-          pass
